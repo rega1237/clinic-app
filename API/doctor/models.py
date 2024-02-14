e2bb6028fa84a6203d0e0b277ca9f6a71bee052f
rega1237 <rega1237@gmail.com>
2024-02-14 08:15:00 -0400

    create doctor model

--- API/doctor/models.py
+++ API/doctor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+class Doctor(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  phone_number = models.CharField(max_length=15)
+  medical_college_reg_number = models.CharField(max_length=20)
+
+  def __str__(self):
+    return f"Dr. {self.user.first_name} {self.user.last_name} - {self.medical_college_reg_number}"
