949206506e28cbe641e9e2134a9d533d28e76580
rega1237 <rega1237@gmail.com>
2024-02-14 08:22:15 -0400

    View to create doctor

--- API/doctor/views.py
+++ API/doctor/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DoctorSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Doctor
 
-# Create your views here.
+class DoctorCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = DoctorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
