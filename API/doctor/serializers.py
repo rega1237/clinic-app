eb974f66f649fa5d63140f74c61dd97bd6812676
rega1237 <rega1237@gmail.com>
2024-02-14 12:04:52 -0400

    combine userSerializar to update doctor data

--- API/doctor/serializers.py
+++ API/doctor/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
 from .models import Doctor
+from authentication.models import User
 
-class DoctorSerializer(serializers.ModelSerializer):
+class DoctorCreateSerializer(serializers.ModelSerializer):
   class Meta:
     model = Doctor
     fields = ["phone_number", "medical_college_reg_number"]
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ["email", "first_name", "last_name"]
+
+class DoctorSerializer(serializers.ModelSerializer):
+  user = UserSerializer()
+  
+  class Meta:
+    model = Doctor
+    fields = ["user", "phone_number", "medical_college_reg_number"]
+
+  def update(self, instance, validated_data):
+    user_data = validated_data.pop('user')
+    user = instance.user
+    user.email = user_data.get('email', user.email)
+    user.first_name = user_data.get('first_name', user.first_name)
+    user.last_name = user_data.get('last_name', user.last_name)
+    user.save()
+    instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+    instance.medical_college_reg_number = validated_data.get('medical_college_reg_number', instance.medical_college_reg_number)
+    instance.save()
+    return instance
