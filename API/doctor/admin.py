db730371b56356a864451a491616fb67f9a6b854
rega1237 <rega1237@gmail.com>
2024-02-17 08:00:45 -0400

    add specilization to the admin panel

--- API/doctor/admin.py
+++ API/doctor/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Doctor
+from .models import Doctor, Specialization
 
 # Register your models here.
 
-admin.site.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+  list_display = ['user', 'phone_number', 'medical_college_reg_number', 'get_specializations']
+
+  def get_specializations(self, obj):
+    return ", ".join([spec.name for spec in obj.specializations.all()])
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Specialization)
