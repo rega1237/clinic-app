97da4c4b45c2450569604416559c5c45b5896b80
rega1237 <rega1237@gmail.com>
2024-02-17 08:00:34 -0400

    views Specialization

--- API/doctor/views.py
+++ API/doctor/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import DoctorCreateSerializer, DoctorSerializer
+from .serializers import DoctorCreateSerializer, DoctorSerializer, SpecializationSerializer
 from rest_framework.permissions import IsAuthenticated
-from .models import Doctor
+from .models import Doctor, Specialization
 
 class DoctorCRUDView(APIView):
     permission_classes = [IsAuthenticated]
@@ -33,3 +33,15 @@ class DoctorCRUDView(APIView):
         doctor.delete()
         request.user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class SpecializationCRUDView(APIView):
+    def get(self, request):
+        serializer = SpecializationSerializer(Specialization.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SpecializationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
