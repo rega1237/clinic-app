8533bddcfeabd18cbf87b1cf80a1e01684edc0d4
rega1237 <rega1237@gmail.com>
2024-02-17 07:59:02 -0400

    create Specialization model

--- API/doctor/models.py
+++ API/doctor/models.py
@@ -9,3 +9,10 @@ class Doctor(models.Model):
 
   def __str__(self):
     return f"Dr. {self.user.first_name} {self.user.last_name} - {self.medical_college_reg_number}"
+  
+class Specialization(models.Model):
+  name = models.CharField(max_length=100)
+  doctors = models.ManyToManyField(Doctor, related_name='specializations', blank=True)
+
+  def __str__(self):
+    return self.name
